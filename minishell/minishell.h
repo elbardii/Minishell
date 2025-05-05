16ae157690c0f1423f542144afed838d93e902bb
yassiryt <yassir.tana@gmail.com>
2025-05-05 18:54:19 +0100

    setup files && main loop

---
+++ minishell/minishell.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yatanagh <yatanagh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/05 18:09:46 by yatanagh          #+#    #+#             */
+/*   Updated: 2025/05/05 18:35:30 by yatanagh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <string.h>
+# include <dirent.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <termios.h>
+# include <term.h>
+# include <sys/ioctl.h>
+# include "libft.h"
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
